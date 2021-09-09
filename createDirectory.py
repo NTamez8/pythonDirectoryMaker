d8e07f48cfaf93dd0ad5f40cdc98ca9c579a01d1
nicolas <nicolas.tamez@icloud.com>
2021-09-09 14:38:37 -0400

    added comments

--- createDirectory.py
+++ createDirectory.py
@@ -3,11 +3,19 @@ from pathlib import Path, PurePath
 
 
 def createDirectory(directoryPath,directoryName):
+    '''
+    directoryPath: the path where the directory should be made
+    directoryName: the name of the new directory
+    '''
     newPath = Path(directoryPath,directoryName)
     if not newPath.exists():
         newPath.mkdir(parents=True)
 
 if __name__ == '__main__':
+    '''
+    assumes that arguments are passed in as pairs
+    so path then folder name
+    '''
     arguments = sys.argv[1:]
     if not len(arguments) % 2 == 0:
         print("To few arguments")
